5dac5beb73823f5df0c8f6a72b58a39a5b63a484
Zac <zacharyflynn06@gmail.com>
2021-08-04 11:36:38 -0600

    commit tree f454642e5a2677e73e42c5beb03aeee6574b92f1

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -4,7 +4,14 @@ class RestaurantInput extends Component {
   render() {
     return (
       <div>
-        Restaurant Input
+        <form>
+
+          <input 
+            type="text" 
+            name="" 
+            id="" />
+          <input type="submit"/>
+        </form>
       </div>
     );
   }
